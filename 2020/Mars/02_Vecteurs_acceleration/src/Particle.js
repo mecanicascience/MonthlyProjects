9986640c6e00e33b8fd1ed61cb9952c7188997a2
xam4lor <xam4lor@gmail.com>
2020-02-09 20:59:47 +0100

    Resized arrows.

--- 2020/Mars/02_Vecteurs_acceleration/src/Particle.js
+++ 2020/Mars/02_Vecteurs_acceleration/src/Particle.js
@@ -23,15 +23,19 @@ class Particle {
                 // Calcul de l'accélération de ce point
                 let v = Vector.sub(this.point.pos, everyObjects[obj].point.pos);
                 let dist = Vector.dist(this.point.pos, everyObjects[obj].point.pos);
+                let val = -1 / (dist * dist * dist);
 
-                this.litteAccel.push(v.mult(-1 / (dist * dist * dist) * 300).limit(1, 5)); // valeurs non-réelle pour la visualisation
+                // pour 'scaler' les vecteurs à des fins de visualisation
+                if(!this.max || -val > this.max) this.max = -val;
+
+                this.litteAccel.push(v.mult(val)); // valeurs non-réelle pour la visualisation
             }
         }
 
         // calcul de l'accélération globale
         this.acceleration.set(0, 0);
         for (let obj = 0; obj < this.litteAccel.length; obj++)
-            this.acceleration.add(this.litteAccel[obj]);
+            this.acceleration.add(this.litteAccel[obj].div(this.max));
 
         noLoop();
     }
@@ -44,8 +48,9 @@ class Particle {
 
         // Affichage de chaque vecteur force (en gris)
         for (let i = 0; i < this.litteAccel.length; i++) {
-            this.litteAccel[i].color = "gray";
-            this.litteAccel[i].draw(this.point.pos);
+            let v = this.litteAccel[i].copy();
+            v.color = "gray";
+            v.draw(this.point.pos);
         }
 
         // Affichage de l'accélération globale
