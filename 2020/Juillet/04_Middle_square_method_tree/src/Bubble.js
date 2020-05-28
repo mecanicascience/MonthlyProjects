c3702d6dd7de3244f4f79595c29fc476d0812d9a
xam4lor <xam4lor@gmail.com>
2020-05-29 00:09:38 +0200

    Added Middle Square Number picker tree generator animation.

---
+++ 2020/Juillet/04_Middle_square_method_tree/src/Bubble.js
@@ -0,0 +1,47 @@
+class Bubble {
+    constructor(x, y, number, isInitial = false) {
+        this.initialPos = new Vector(x, y);
+
+        this.pos = this.initialPos.copy();
+        //this.pos.add(2, 2);
+        this.vel = new Vector(0.1, 0);
+        this.acc = new Vector(0, 0);
+
+        this.number = new pSText(number >= 10 ? number : '0' + number, (this.pos.copy()).sub(0, 0), 28, 'black');
+
+        this.isInitial = isInitial;
+    }
+
+    update(dt, cursorPos) {
+        this.acc.clear();
+
+        let k = [3, 500, 0.02];
+
+        // Mouse Avoiding Target
+        let r = Vector.dist(this.pos, cursorPos);
+        this.acc.add(((this.pos.copy()).sub(cursorPos)).mult(1 / r**3 * k[0]));
+
+        // Hooke force
+        let r2 = Vector.dist(this.initialPos, this.pos);
+        this.acc.add((this.initialPos.copy()).sub(this.pos).mult(k[1] * r2));
+
+        // Friction
+        this.acc.add((this.vel.copy()).mult(-k[2] * this.vel.mag()));
+
+        this.vel.add(this.acc.mult(dt));
+        this.pos.add((this.vel.copy()).mult(dt*dt));
+
+        this.number.setPosition(this.pos.x - 0.02, this.pos.y - 0.1);
+    }
+
+    draw(drawer) {
+        drawer
+            .fill(this.isInitial ? 'rgba(20, 150, 20, 1)' : 'rgba(120, 120, 120, 1)')
+            .stroke('white')
+            .strokeWeight(2)
+            .ellipse(this.pos.x, this.pos.y, 40, 40)
+        ;
+
+        this.number.draw();
+    }
+}
